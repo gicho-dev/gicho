7bc82f3a972acf429a24a546795989bc76607941
Lucas Choi <eterv7@gmail.com>
2025-04-25 22:48:23 +0900

    initial setting

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+
+/** @type {import('prettier').Config} */
+const config = {
+	semi: false,
+	singleQuote: true,
+	trailingComma: 'all',
+	printWidth: 100,
+	useTabs: true,
+}
+
+export default config
