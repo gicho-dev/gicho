f89d2759d64aa64ee8ce93800b04439d135870e1
Lucas Choi <eterv7@gmail.com>
2025-06-12 17:26:02 +0900

    fix(core): 🐛 update index.ts, package.json (for tsconfig customConditions)

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,8 @@
+export * from './fetch'
+export * from './types'
+export * from './url'
+
+/** @deprecated this is a test. so will be removed in next version */
 export function hello(name: string): string {
 	return `Hello, ${name}!`
 }
