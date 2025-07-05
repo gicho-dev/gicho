e03462375147439a0eeb84c57f7cff83ced444c6
Lucas Choi <eterv7@gmail.com>
2025-07-05 23:15:34 +0900

    chore: adjust eslint config for tests

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,25 +1,43 @@
 import { config } from './packages/config/src/eslint'
 
-export default config({
-	disables: {
-		prettier: true,
-	},
+export default config(
+	{
+		disables: {
+			prettier: true,
+		},
 
-	ignores: {
-		customIgnores: ['**/.local'],
-	},
+		ignores: {
+			customIgnores: ['**/.local'],
+		},
 
-	json: {
-		sortPackageJson: true,
-		sortTsconfigJson: true,
-	},
+		json: {
+			sortPackageJson: true,
+			sortTsconfigJson: true,
+		},
 
-	typescript: {
-		rules: {
-			'@typescript-eslint/explicit-function-return-type': [
-				'error',
-				{ allowExpressions: true, allowHigherOrderFunctions: true, allowIIFEs: true },
-			],
+		typescript: {
+			rules: {
+				'@typescript-eslint/explicit-function-return-type': [
+					'error',
+					{ allowExpressions: true, allowHigherOrderFunctions: true, allowIIFEs: true },
+				],
+			},
 		},
 	},
-})
+
+	{
+		name: 'gicho/tests',
+		files: ['**/*.{bench,fixture,spec,test}.ts', '**/tests/**/*.js'],
+		rules: {
+			'no-cond-assign': 'off',
+			'no-object-constructor': 'off',
+			'no-proto': 'off',
+			'no-prototype-builtins': 'off',
+			'no-restricted-properties': 'off',
+			'no-useless-assignment': 'off',
+			'@typescript-eslint/explicit-function-return-type': 'off',
+			'@typescript-eslint/no-extraneous-class': 'off',
+			'@typescript-eslint/no-unsafe-function-type': 'off',
+		},
+	},
+)
