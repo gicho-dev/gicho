683509241e69a065bd949b6de791866099051735
Lucas Choi <eterv7@gmail.com>
2025-07-05 23:08:55 +0900

    build(core): update tsup config entries

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,7 +1,3 @@
-export * from './fetch'
-export * from './types'
-export * from './url'
-
 /** @deprecated this is a test. so will be removed in next version */
 export function hello(name: string): string {
 	return `Hello, ${name}!`
--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -3,7 +3,16 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
 	clean: true,
 	dts: true,
-	entry: { index: 'src/index.ts' },
+	entry: {
+		index: 'src/index.ts',
+		...['fetch', 'is', 'object', 'types'].reduce(
+			(obj, group) => {
+				obj[group] = `src/${group}/index.ts`
+				return obj
+			},
+			{} as Record<string, string>,
+		),
+	},
 	format: ['esm'],
 	outDir: 'dist',
 	// shims: true,
