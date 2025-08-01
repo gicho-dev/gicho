85f62756e858ae73d6a4010b3001f7611223d68e
Lucas Choi <eterv7@gmail.com>
2025-08-01 10:27:45 +0900

    feat!: remove config project
    moved to gicho-dev/config respostory

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,44 +1,14 @@
-import { config } from './packages/config/src/eslint'
+import { defineConfig } from '@gicho/config/eslint'
 
-export default config(
-	{
-		disables: {
-			prettier: true,
-		},
-
-		ignores: {
-			customIgnores: ['**/.local'],
-		},
-
-		json: {
-			sortPackageJson: true,
-			sortTsconfigJson: true,
-		},
-
-		typescript: {
-			rules: {
-				'@typescript-eslint/explicit-function-return-type': [
-					'error',
-					{ allowExpressions: true, allowHigherOrderFunctions: true, allowIIFEs: true },
-				],
-			},
-		},
+export default defineConfig({
+	ignores: {
+		customIgnores: ['**/.local'],
 	},
 
-	{
-		name: 'gicho/tests',
-		files: ['**/*.{bench,fixture,spec,test}.ts', '**/tests/**/*.js'],
-		rules: {
-			'no-cond-assign': 'off',
-			'no-object-constructor': 'off',
-			'no-proto': 'off',
-			'no-prototype-builtins': 'off',
-			'no-restricted-properties': 'off',
-			'no-useless-assignment': 'off',
-			'@typescript-eslint/explicit-function-return-type': 'off',
-			'@typescript-eslint/no-extraneous-class': 'off',
-			'@typescript-eslint/no-unsafe-function-type': 'off',
-			'@typescript-eslint/no-unused-expressions': 'off',
-		},
+	json: {
+		sortPackageJson: true,
+		sortTsconfigJson: true,
 	},
-)
+
+	svelte: false,
+})
--- packages/config/tsdown.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsdown'
-
-export default defineConfig({
-	dts: true,
-	entry: {
-		eslint: 'src/eslint/index.ts',
-		prettier: 'src/prettier/index.ts',
-	},
-})
